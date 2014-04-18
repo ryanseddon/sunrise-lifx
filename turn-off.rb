d0e9003d12cafb8137483fb752c2aa55782d91a9
Ryan Seddon <seddon.ryan@gmail.com>
2014-04-18 13:34:37 +1000

    Refactor turn off script to accept argument

--- turn-off.rb
+++ turn-off.rb
@@ -1,15 +1,16 @@
 require 'lifx'
 
+label = ARGV[0]
+
 client = LIFX::Client.lan
 client.discover! do |c|
-    c.lights.with_label('Bedroom')
+    c.lights.with_label(label)
 end
 
 client.lights.turn_on
-light = client.lights.with_label('Bedroom')
+light = client.lights.with_label(label)
 
-green = LIFX::Color.hsl(0, 1, 0.01)
-light.set_color(green, duration: 1)
+light.set_color(LIFX::Color.hsl(0, 1, 0.01), duration: 1)
 
 sleep 1
 client.lights.turn_off
--- turn-off.sh
+++ turn-off.sh
@@ -2,4 +2,4 @@
 
 source ~/.rvm/scripts/rvm
 cd ~/sunrise-lifx
-bundle exec ruby ~/sunrise-lifx/turn-off.rb
+bundle exec ruby ~/sunrise-lifx/turn-off.rb "Bedroom"
